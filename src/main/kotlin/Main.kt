3e8cf67f57ac3be0965b6195b7aeda0ae04acef6
BurnWilD <76895997+BurnWilD@users.noreply.github.com>
2024-04-11 18:51:35 +0800

    unicorn corn

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,16 @@
 package org.example
 
+
+
 fun main() {
     var hasSteed=false
     println("Удалось герою обзавестись боевым конем - $hasSteed")
+    /*
     hasSteed=true
-    println("Удалось герою обзавестись боевым конем - $hasSteed")
+    println("Удалось герою обзавестись боевым конем - $hasSteed")*/
+    val nameBar="Рог единорога"
+    val nameBarmen="Стивен"
+    var valueMoney=50
+    var menu= listOf("Мёд","Вино","Пиво")
 
 }
