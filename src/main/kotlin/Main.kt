68928ac383f6632414e5fefc9c15f9eecfe33301
BurnWilD <76895997+BurnWilD@users.noreply.github.com>
2024-04-11 17:53:27 +0800

    Education_two

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,12 @@
 package org.example
-
+const val HERO_NAME ="Madrigal"
 fun main() {
-    println("Hello World!")
 
+    println(HERO_NAME)
+    var playerLevel =4
+    println(playerLevel)
+
+    println("The hero embarks on her journey to locate the enchanted sword.")
+    playerLevel++
+    println(playerLevel)
 }
