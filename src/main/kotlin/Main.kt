c2a5d30dbdef91ad88cc18e16c56d3af4b646f79
BurnWilD <76895997+BurnWilD@users.noreply.github.com>
2024-04-12 14:47:08 +0800

    if/else/when

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,16 +3,7 @@ package org.example
 
 const val HERO_NAME="Madrigal"
 fun main() {
-    var hasSteed=false
-    println("Удалось герою обзавестись боевым конем - $hasSteed")
-    /*
-    hasSteed=true
-    println("Удалось герою обзавестись боевым конем - $hasSteed")*/
-    val nameBar="Рог единорога"
-    val nameBarmen="Стивен"
-    var valueMoney=50
-    var menu= listOf("Мёд","Вино","Пиво")
-    println("Герой воспользовался волшебным зеркалом, теперь его имя: ${HERO_NAME.reversed()}")
+
     val playerlevel=4
     val hasBeFriendedBarbarians=true
     val hasAngeredBarbarians=false
